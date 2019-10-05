3a12b00141f0cabd7fda315528ef569bff787212
Prakhar Srivastava <softzon21@gmail.com>
2019-10-05 20:00:02 +0200

    Refactored SourceAPI to let it have Checkable models instead

--- src/RestHandle/Request/SourcesAPI.cpp
+++ src/RestHandle/Request/SourcesAPI.cpp
@@ -13,50 +13,8 @@ SourcesAPI::SourcesAPI(QObject* parent)
 {
 }
 
-QString SourcesAPI::country() const
-{
-    return m_country;
-}
-
-QString SourcesAPI::category() const
-{
-    return m_category;
-}
-
-QString SourcesAPI::language() const
-{
-    return m_language;
-}
-
 QUrl SourcesAPI::prepareRequest()
 {
     // prepare the actual request here with all the query parameters
     return QUrl(QLatin1String(ApiEndpoint::URL_SOURCES));
-}
-
-void SourcesAPI::setCountry(QString country)
-{
-    if (m_country == country)
-        return;
-
-    m_country = country;
-    emit countryChanged();
-}
-
-void SourcesAPI::setCategory(QString category)
-{
-    if (m_category == category)
-        return;
-
-    m_category = category;
-    emit categoryChanged();
-}
-
-void SourcesAPI::setLanguage(QString language)
-{
-    if (m_language == language)
-        return;
-
-    m_language = language;
-    emit languageChanged();
 }
--- src/RestHandle/Request/SourcesAPI.h
+++ src/RestHandle/Request/SourcesAPI.h
@@ -4,31 +4,10 @@
 
 class SourcesAPI : public QObject {
     Q_OBJECT
-
-    Q_PROPERTY(QString country READ country WRITE setCountry NOTIFY countryChanged)
-    Q_PROPERTY(QString category READ category WRITE setCategory NOTIFY categoryChanged)
-    Q_PROPERTY(QString language READ language WRITE setLanguage NOTIFY languageChanged)
+    // Add country, category, language models
 
 public:
     explicit SourcesAPI(QObject* parent = nullptr);
 
-    QString country() const;
-    QString category() const;
-    QString language() const;
     QUrl prepareRequest();
-
-public slots:
-    void setCountry(QString country);
-    void setCategory(QString category);
-    void setLanguage(QString language);
-
-signals:
-    void countryChanged();
-    void categoryChanged();
-    void languageChanged();
-
-private:
-    QString m_country;
-    QString m_category;
-    QString m_language;
 };
